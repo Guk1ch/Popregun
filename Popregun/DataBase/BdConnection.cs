bad49073f0de116ca062fc5a88ae64a7a16b3696
201903 <201903@TOCHKA-008>
2023-03-06 18:11:26 +0300

    Добавьте файлы проекта.

---
+++ Popregun/DataBase/BdConnection.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Popregun.DataBase
+{
+	public class BdConnection
+	{
+		public static PopryzjekEntities connection = new PopryzjekEntities();
+	}
+}
